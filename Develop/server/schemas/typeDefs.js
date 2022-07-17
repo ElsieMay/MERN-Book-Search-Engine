ee83c5fd718186cb9f2d79b8320f4c7850de0c05
ElsieMay <elsiemaylawrie1@gmail.com>
2022-07-17 15:40:49 +1000

    added mutation to typeDefs

--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -34,6 +34,12 @@ const typeDefs = gql`
 		token: ID!
 		user: User
 	}
+	type Mutation {
+		addUser(username: String!, email: String!, password: String!): Auth
+		login(email: String!, password: String!): Auth
+		saveBook(input: savedBook!): User
+		deleteBook(bookId: ID!): User
+	}
 `;
 // export the typeDefs
 module.exports = typeDefs;
