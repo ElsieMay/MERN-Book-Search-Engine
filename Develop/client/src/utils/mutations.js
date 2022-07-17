3eaf5f0c013f815affb1eb63ca1ffd6faf399f93
ElsieMay <elsiemaylawrie1@gmail.com>
2022-07-17 17:58:56 +1000

    added mutation to add a new user

--- Develop/client/src/utils/mutations.js
+++ Develop/client/src/utils/mutations.js
@@ -1,8 +1,19 @@
 import { gql } from "@apollo/client";
 
 export const LOGIN_USER = gql`
-	mutation login($email: String!, $password: String!) {
+	mutation loginUser($email: String!, $password: String!) {
 		login(email: $email, password: $password) {
+			token
+			user {
+				_id
+				username
+			}
+		}
+	}
+`;
+export const ADD_USER = gql`
+	mutation addUser($username: String!, $email: String!, $password: String!) {
+		login(username: $username, email: $email, password: $password) {
 			token
 			user {
 				_id
